86b544b1b03a7fc6dcc04d7609586eb61b986e6e
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-07-17 13:53:15 +0800

    [feat:]用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,9 @@ Route::group(['middleware' => 'auth'], function () {
 
         // 申请退款
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+        // 优惠券
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     });
 });
 // 支付宝服务端回调
